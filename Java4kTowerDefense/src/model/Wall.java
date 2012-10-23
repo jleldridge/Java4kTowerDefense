735ea0b52b4ee146fe92161a6dce7767ac6f2d1f
Luke Eldridge <jleldrid@uncg.edu>
2012-10-23 16:08:17 -0400

    Removed test comment.

--- Java4kTowerDefense/src/model/Wall.java
+++ Java4kTowerDefense/src/model/Wall.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import engine.Engine;
-// test - hrybacki -- testing github
+
 public class Wall implements Entity {
 
 	//fields
