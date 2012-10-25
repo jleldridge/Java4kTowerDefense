77c6d7e3e7b4102b117fffb6e5bac7a4f78d8a6c
hrybacki <hrybacki@gmail.com>
2012-10-25 19:33:29 -0400

    Didn't extend new viewable interface.

--- Java4kTowerDefense/src/model/Entity.java
+++ Java4kTowerDefense/src/model/Entity.java
@@ -2,17 +2,8 @@ package model;
 
 import java.awt.image.BufferedImage;
 
-public interface Entity {
-	//start out with blue squares for the player, green for terrain objects, red for enemies
-	//squares should be 10x10 pixels
-	//for getting the current frame of an animation
-	public BufferedImage getImage();
+abstract class Entity implements Viewable {
 	
-	//for getting the position on the grid
-	public int getX();
-	public void setX(int x);
-	public int getY();
-	public void setY(int y);
 	
 }
 
