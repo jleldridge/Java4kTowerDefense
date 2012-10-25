7b35fa0a294154973e7fb2eb93fb0f486183aba6
Luke Eldridge <jleldrid@uncg.edu>
2012-10-24 23:53:19 -0400

    Added code to handle key input by pushing the values of keys pressed onto a stack when a key is pressed and it's not already on the stack and removing it from the stack when it is released, and then performing the action mapped to the key on the top of the stack (like changing the player's speed) in the game loop. The result is very smooth motion compared to the old way of doing things, and will get smoother when it takes 10 frames to traverse a single block.

--- Java4kTowerDefense/src/engine/Engine.java
+++ Java4kTowerDefense/src/engine/Engine.java
@@ -2,6 +2,8 @@ package engine;
 
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Stack;
 
 import model.Entity;
 import model.Player;
@@ -9,86 +11,86 @@ import model.Wall;
 
 public class Engine {
 	public final static int SQUARE_DIMENSION = 25;
-	public final static int GRID_HEIGHT = 600/25;
-	public final static int GRID_WIDTH = 800/25;
+	public final static int GRID_HEIGHT = 600 / 25;
+	public final static int GRID_WIDTH = 800 / 25;
+	// HashSet which holds the keys that are currently down.
+	Stack<Integer> keysDown;
 	Entity[][] gameArea;
 	Player player;
-	
-	public Engine(){
-		//going to fill the grid with Tile types once that class is created
+	int tempCounter = 10;
+
+	public Engine() {
+		// going to fill the grid with Tile types once that class is created
 		gameArea = new Entity[GRID_HEIGHT][GRID_WIDTH];
-		
+		keysDown = new Stack<Integer>();
 		player = new Player();
 		gameArea[player.getY()][player.getX()] = player;
-		
-		//for testing positioning of entities on the grid
-//		gameArea[0][0] = new Wall(0, 0);
-//		gameArea[1][5] = new Wall(1, 5);
-//		gameArea[7][1] = new Wall(7, 1);
-//		gameArea[7][8] = new Wall(7, 8);
-//		gameArea[8][7] = new Wall(8, 7);
-//		gameArea[GRID_HEIGHT-1][GRID_WIDTH-1] = new Wall(GRID_HEIGHT-1, GRID_WIDTH-1);
-		
-		
+
+		// for testing positioning of entities on the grid
+		// gameArea[0][0] = new Wall(0, 0);
+		// gameArea[1][5] = new Wall(1, 5);
+		// gameArea[7][1] = new Wall(7, 1);
+		// gameArea[7][8] = new Wall(7, 8);
+		// gameArea[8][7] = new Wall(8, 7);
+		// gameArea[GRID_HEIGHT-1][GRID_WIDTH-1] = new Wall(GRID_HEIGHT-1,
+		// GRID_WIDTH-1);
+
 	}
-	
-	public void update(){
-		//delete the player's old position on the grid
+
+	public void update() {
+		// check which key is the latest one pressed and still down and
+		// modify the player's action states appropriately
+		int code = 0;
+		if (!keysDown.empty()) {
+			code = keysDown.peek();
+		}
+		if (code == KeyEvent.VK_LEFT) {
+			player.setDx(-1);
+		} else if (code == KeyEvent.VK_RIGHT) {
+			player.setDx(1);
+		} else if (code == KeyEvent.VK_UP) {
+			player.setDy(-1);
+		} else if (code == KeyEvent.VK_DOWN) {
+			player.setDy(1);
+		}
+
+		// delete the player's old position on the grid
 		gameArea[player.getY()][player.getX()] = null;
-		
-		//set the player's position based on the dx and dy values
+
+		// set the player's position based on the dx and dy values
 		player.setX(player.getX() + player.getDx());
 		player.setY(player.getY() + player.getDy());
-		
-		//place the player onto the grid based on its new x and y values
+
+		// place the player onto the grid based on its new x and y values
 		gameArea[player.getY()][player.getX()] = player;
+
+		// reset the action states of the player
+		player.setDx(0);
+		player.setDy(0);
 	}
-	
-	public void render(Graphics g){
-		for(int i = 0; i < GRID_HEIGHT; i++){
-			for(int j = 0; j < GRID_WIDTH; j++){
-				if(gameArea[i][j] != null)
-					g.drawImage(gameArea[i][j].getImage(), SQUARE_DIMENSION*j, SQUARE_DIMENSION*i, null);
+
+	public void render(Graphics g) {
+		for (int i = 0; i < GRID_HEIGHT; i++) {
+			for (int j = 0; j < GRID_WIDTH; j++) {
+				if (gameArea[i][j] != null)
+					g.drawImage(gameArea[i][j].getImage(),
+							SQUARE_DIMENSION * j, SQUARE_DIMENSION * i, null);
 			}
 		}
 	}
-	
-	public void keyPressed(KeyEvent e){
+
+	public void keyPressed(KeyEvent e) {
 		int code = e.getKeyCode();
-		
-		switch(code){
-		case KeyEvent.VK_UP:
-			player.setDy(-1);
-			break;
-		case KeyEvent.VK_DOWN:
-			player.setDy(1);
-			break;
-		case KeyEvent.VK_LEFT:
-			player.setDx(-1);
-			break;
-		case KeyEvent.VK_RIGHT:
-			player.setDx(1);
-			break;
+		if (!keysDown.contains(code)) {
+			keysDown.push(code);
 		}
 	}
-	
-	public void keyReleased(KeyEvent e){
+
+	public void keyReleased(KeyEvent e) {
 		int code = e.getKeyCode();
-		
-		switch(code){
-		case KeyEvent.VK_UP:
-			player.setDy(0);
-			break;
-		case KeyEvent.VK_DOWN:
-			player.setDy(0);
-			break;
-		case KeyEvent.VK_LEFT:
-			player.setDx(0);
-			break;
-		case KeyEvent.VK_RIGHT:
-			player.setDx(0);
-			break;
-		}
-		
+		// have to use Integer.valueOf() so that we get the
+		// stack.remove(Object o) method instead of the
+		// stack.remove(int index) method
+		keysDown.remove(Integer.valueOf(code));
 	}
 }
