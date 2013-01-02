045312b484d3deb52375c4f8dcc4c28373273304
hrybacki <hrybacki@gmail.com>
2013-01-01 23:05:25 -0500

    New mobs for testing.

---
+++ Java4kTowerDefense/src/model/Entity/Blob.java
@@ -0,0 +1,37 @@
+package model.Entity;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import engine.Engine;
+
+public class Blob extends Entity{
+	private BufferedImage sprite;
+	private int movementFrame;
+	
+	public Blob(){
+		setX(0);
+		setY(0);
+		sprite = new BufferedImage(Engine.SQUARE_DIMENSION, Engine.SQUARE_DIMENSION, BufferedImage.TYPE_INT_RGB);
+		Graphics g = sprite.getGraphics();
+		g.setColor(Color.gray);
+		g.fillRect(6, 6, Engine.SQUARE_DIMENSION/2, Engine.SQUARE_DIMENSION/2);
+		movementFrame = 0;
+	}
+	
+	public int getMovementFrame(){
+		return movementFrame;
+	}
+	
+	public void nextMovementFrame(){
+		movementFrame = movementFrame < 9 ? movementFrame+1 : 0;
+	}
+	
+	@Override
+	public BufferedImage getImage() {
+		//return some array[movementFrame], where some array is the
+		//array holding the frames for this direction of movement.
+		return sprite;
+	}
+}
