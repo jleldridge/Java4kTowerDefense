c2e34e14efd53e764aa6613c23b00df25e7c7b4e
hrybacki <hrybacki@gmail.com>
2013-01-01 22:44:24 -0500

    Entities no longer needed in tiles.

--- Java4kTowerDefense/src/model/Tile.java
+++ Java4kTowerDefense/src/model/Tile.java
@@ -10,27 +10,22 @@ import model.Terrain.Terrain;
 public class Tile {
 	// fields
 	Terrain terr;
-	Entity ent;
 	Effect eff;
 	
 	// constructor
 	public Tile() {
 		this.terr = null;
-		this.ent = null;
 		this.eff = null;
 	}
 	
-	public Tile(Terrain terr, Entity ent, Effect eff) {
+	public Tile(Terrain terr, Effect eff) {
 		this.terr = terr;
-		this.ent = ent;
 		this.eff = eff;
 	}
 	
 	// setter/getters
 	public void setTerrain(Terrain terr) { this.terr = terr; }
 	public Terrain getTerrain() { return this.terr; }
-	public void setEntity(Entity ent) { this.ent = ent; }
-	public Entity getEntity() { return this.ent; }
 	public void setEffect(Effect eff) { this.eff = eff; }
 	public Effect getEffect() { return this.eff; }
 	public Tile getTile() { return this; }
